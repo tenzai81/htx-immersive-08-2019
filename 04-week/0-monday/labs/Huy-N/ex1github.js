bb4017261207d9bca6cdf48e2506c942a68cb908
Huy Nguyen <tenzai81@gmail.com>
2019-09-17 11:44:18 -0500

    new

---
+++ 04-week/0-monday/labs/Huy-N/ex1github.js
@@ -0,0 +1,63 @@
+// ex 1
+// function lvl1exercise1 () {
+//     // Declare a variable without instantiating it and return it!
+  
+var firstName;
+firstName = "Huy"
+console.log(firstName)
+
+// // ex 2
+// function lvl1exercise2 () {
+//     // Declare and instantiate a number and return it
+  
+
+var age = 13;
+console.log(age)
+
+// ex 3
+// function lvl1exercise3 () {
+//     // Declare and instantiate a floating point number that is not a whole number and return it
+  
+//   }
+var decimal = 4.3;
+console.log(decimal)
+
+// ex 4
+// function lvl1exercise4 () {
+//     // Declare and instantiate a string "Hello World!" and return it
+  
+//   }
+
+var greetings = "Hello World!"
+console.log(greetings)
+
+// ex 5
+// function lvl1exercise5 () {
+//     // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+  
+//   }
+
+var someGreetings = "Hello World " + 4
+console.log(someGreetings)
+
+// ex6
+// function lvl1exercise6 () {
+//     // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+  
+//   }
+
+var characterSheet = {
+    greetings: 'Hello World! ',
+    age: 5
+};   
+
+console.log(characterSheet)
+
+// ex7
+// function lvl1exercise7 () {
+//     // Declare and instantiate a boolean value 'false' and return it
+  
+//   }
+
+var underAge = false;
+console.log(underAge)
