8d6c6700ba7daa5a197e82ed02e95442ac11f396
Huy Nguyen <tenzai81@gmail.com>
2019-09-17 12:04:26 -0500

    level 3

---
+++ 04-week/0-monday/labs/Huy-N/level2.js
@@ -0,0 +1,36 @@
+
+// ex1.
+// function lvl2exercise1 (num1, num2) {
+//     // Return the sum of num1 and num2
+  
+var num1 = 9383
+var num2 = 83733
+
+console.log(num1 + num2)
+// ex2
+// function lvl2exercise2 (num1, num2) {
+//     // Return the difference of num1 and num2
+  
+//   }
+
+console.log(num1 - num2)
+
+// ex3
+// function lvl2exercise3 (num1, num2) {
+//     // Return the multiplication of num1 and num 2
+  
+console.log(num1*num2)
+
+// ex4
+// function lvl2exercise4 (num1, num2) {
+//     // Return the division of num1 and num2
+  
+//   }
+console.log(num1/num2)
+
+// ex5
+// function lvl2exercise5 (num1) {
+//     // Add 2 to num1 using += and return it
+  
+num1 += 2;
+console.log(num1)
---
+++ 04-week/0-monday/labs/Huy-N/level3.js
@@ -0,0 +1,29 @@
+// function lvl3exercise1 () {
+//     // Create a "hello" and a "world", return the concatenation of the two
+var greetings = "hello";
+var secondGreetings = "world";
+
+console.log( greetings + " " + secondGreetings)
+//   function lvl3exercise2 () {
+//     // Create a "hello" and a 12, return the concatenation of the two
+  
+var number = 12;
+console.log( greetings + " " + number)
+
+  
+//   function lvl3exercise3 () {
+//     // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
+
+console.log("The number " + number + " is an integer " )
+  
+//   function lvl3exercise4 () {
+//     // Create a "hello world!" string. Return the length of the string
+
+console.log(greetings.length)
+  
+//   function lvl3exercise5 () {
+//     // Create a "hello world!" string. Return the index of the word "world".
+  
+var greet = "Hello World";
+console.log(greet.indexOf('World'))
+
