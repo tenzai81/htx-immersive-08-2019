0c492c81e3db70d56c7aec14157be6f32923b05b
Huy Nguyen <tenzai81@gmail.com>
2019-09-17 12:36:15 -0500

    level4

---
+++ 04-week/0-monday/labs/Huy-N/level4.js
@@ -0,0 +1,28 @@
+// function lvl4exercise1 () {
+//     // Return the boolean value "true"
+  
+//   }
+  
+//   function lvl4exercise2 () {
+//     // Return the boolean value "false"
+  
+console.log(false)
+  
+//   function lvl4exercise3 (bool) {
+//     // Return the opposite of the input boolean value
+  
+console.log(!false)
+  
+//   function lvl4exercise4 (bool1, bool2) {
+//     // Return the logical "and" of the input boolean values
+var bool1 = true;
+var bool2 = false;
+console.log(bool1 && bool2)
+  
+//   function lvl4exercise5 (bool1, bool2) {
+//     // Return the logical "or" of the input boolean values
+console.log(bool1 || bool2)
+  
+//   function lvl4exercise6 (bool1, bool2) {
+//     // Return the logical "equivalence" of the input boolean values
+  console.log(bool1 == bool2)
