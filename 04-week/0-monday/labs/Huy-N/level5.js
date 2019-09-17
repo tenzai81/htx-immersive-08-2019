32562568d53dcbb0cfa4b3727bb0cc8e818e1a27
Huy Nguyen <tenzai81@gmail.com>
2019-09-17 12:42:38 -0500

    level 5

---
+++ 04-week/0-monday/labs/Huy-N/level5.js
@@ -0,0 +1,22 @@
+// function lvl5exercise1 () {
+//     // Push the string "hello" into the array and return it.
+//     var arr = [1, 'adam']
+var array = [ 1, 'adam']
+console.log(array.push("hello"))
+console.log(array)
+  
+//   function lvl5exercise2 () {
+//     // Remove the last element from the array and return it
+//     var arr = [1, 'adam']
+console.log(array.pop())
+console.log(array)
+  
+//   function lvl5exercise3 () {
+//     // Return the length of the array
+//     var arr = [1, 'adam']
+console.log(array.length)
+  
+//   function lvl5exercise4 () {
+//     // Return the index of "adam" in the array
+//     var arr = [1, 'adam']
+console.log(array.indexOf('adam'))
