2c48829e1d59bbdd3e0d43ceb90b1d0d10c72472
Huy Nguyen <tenzai81@gmail.com>
2019-09-17 21:01:39 -0500

    new

---
+++ 04-week/1-tuesday/labs/js101-master/exercises/level1.js
@@ -0,0 +1,38 @@
+function lvl1exercise1 () {
+  // Declare a variable without instantiating it and return it!
+var firstName;
+console.log(firstName);
+}
+
+function lvl1exercise2 () {
+  // Declare and instantiate a number and return it
+var number = 11;
+console.log(number)
+
+
+function lvl1exercise3 () {
+  // Declare and instantiate a floating point number that is not a whole number and return it
+var decimal = 3.4;
+console.log(decimal)
+}
+
+function lvl1exercise4 () {
+  // Declare and instantiate a string "Hello World!" and return it
+var str = "Hello World!";
+console.log(str);
+}
+
+function lvl1exercise5 () {
+  // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+
+}
+
+function lvl1exercise6 () {
+  // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
+
+}
+
+function lvl1exercise7 () {
+  // Declare and instantiate a boolean value 'false' and return it
+
+}
