6db368cbf58b5e3eb32a8b93aa02ac31c227a8d6
Huy Nguyen <tenzai81@gmail.com>
2019-09-17 21:47:25 -0500

    new

---
+++ 04-week/0-monday/labs/Huy-N/printSquare.js
@@ -0,0 +1,8 @@
+var size = 4;
+var i = 0;
+for (i = 0; i < size; i++){
+    for (var x = 0; x < size; x++)
+
+    console.log("*");
+    console.log("\n")
+}
--- 04-week/0-monday/labs/Huy-N/tipcalc.js
+++ 04-week/0-monday/labs/Huy-N/tipcalc.js
@@ -1,18 +1,19 @@
 var bill = prompt("Please enter your total bill: ") ;
    var service = prompt("How was the service?\n Please enter Good, OK, or Bad: ");
-function tipAmount(){
+
+   function tipAmount(){
    
-        if (service = "Good")
+        if (service == "Good")
             var tipValue = bill * .2;
             var totalBill =  bill + tipValue;
             console.log("Your total bill is " + totalBill + "plus a $" + tipValue "tip!")
-        else if (service = "OK")
+        else if (service == "OK")
             var tipValue = bill * .15;
             var totalBill = bill + tipValue;
             console.log("Your total bill is " + totalBill + "plus a $" + tipValue "tip!");
-        else if (service = "Bad")
+        else if (service == "Bad")
         var tipValue = bill * .10;
         var totalBill = bill + tipValue;
         console.log("Your total bill is " + totalBill + "plus a $" + tipValue "tip!");  
 }
-console.log(tipAmount(100));
+
