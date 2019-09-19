7471e6f1a816ab2193f8952ad45ea4cd564a189f
Huy Nguyen <tenzai81@gmail.com>
2019-09-19 14:10:14 -0500

    ex6

---
+++ 04-week/3-thursday/labs/Huy-N/ex6.js
@@ -0,0 +1,7 @@
+
+var people = [ 'Dom', 'Lyn', 'Kirk', 'Autumn', 'Trista', 'Jesslyn', 'Kevin', 'John', 'Eli', 'Juan', 'Robert', 'Keyur', 'Jason', 'Che', 'Ben' ];
+
+var newList = people.map(function(element){
+    return "Good job " + element ;
+});
+console.log(newList)
