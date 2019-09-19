078ebb8edd2f31e40125a344f90e9f71e6e33c69
Huy Nguyen <tenzai81@gmail.com>
2019-09-19 14:26:32 -0500

    ex7

---
+++ 04-week/3-thursday/labs/Huy-N/ex7.js
@@ -0,0 +1,4 @@
+var people = [ 'Dom', 'Lyn', 'Kirk', 'Autumn', 'Trista', 'Jesslyn', 'Kevin', 'John', 'Eli', 'Juan', 'Robert', 'Keyur', 'Jason', 'Che', 'Ben' ];
+people.sort();
+
+console.log(people)
