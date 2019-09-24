8b55ba8ef4050430b02a82a6945f9713b2b93134
Huy Nguyen <tenzai81@gmail.com>
2019-09-24 09:20:10 -0500

    cards updates

--- 05-week/0-monday/labs/Huy-N/blackjack/blackjack.js
+++ 05-week/0-monday/labs/Huy-N/blackjack/blackjack.js
@@ -1,24 +1,23 @@
-// Function to display cards
-function getCardImageUrl(card) {
-    var cardName;
-    if (card.point === 1 or card.point === 10) {
-      cardName = 'ace';
-    } else if (card.point === 11) {
-      cardName = 'jack';
-    } else if (card.point === 12) {
-      cardName = 'queen';
-    } else if (card.point === 13) {
-      cardName = 'king';
-    } else {
-      cardName = card.point;
-    }
-    return 'images/' + cardName + '_of_' + card.suit + '.png';
-  }
-//   Calculate points for cards
 
-
-// Deal new two cards to each player
-
-function dealCards(card){
-    var 
-})
+// Deal new two cards to each player Player and Dealer
+document.getElementById('dealer-button').addEventListener('deal-button', dealCards);
+  function dealCards(){
+        document.getElementById('dealer-hand').innerHTML='<img src="images/ace_of_hearts.png" alt="card1"><img src="images/king_of_clubs.png">';
+        document.getElementById('player-hand').innerHTML='<img src="images/ace_of_hearts.png" alt="card1"><img src="images/king_of_clubs.png">';
+}
+// var deck =[{point: 13; suit: "hearts"}, {point: 13; suit: "diamonds"},{point: 13; suit: "clubs"},{point: 13; suit: "spade"},
+// {point: 12; suit: "hearts"}, {point: 12; suit: "diamonds"},{point: 12; suit: "clubs"},{point: 12; suit: "spade"},
+// {point: 11; suit: "hearts"}, {point: 11; suit: "diamonds"},{point: 11; suit: "clubs"},{point: 11; suit: "spade"}, 
+// {point: 10; suit: "hearts"}, {point: 10; suit: "diamonds"},{point: 10; suit: "clubs"},{point: 10; suit: "spade"}, 
+// {point: 9; suit: "hearts"}, {point: 9; suit: "diamonds"},{point: 9; suit: "clubs"},{point: 9; suit: "spade"},
+// {point: 8; suit: "hearts"}, {point: 8; suit: "diamonds"},{point: 8; suit: "clubs"},{point: 8; suit: "spade"},
+// {point: 7; suit: "hearts"}, {point: 7; suit: "diamonds"},{point: 7; suit: "clubs"},{point: 7; suit: "spade"},
+// {point: 6; suit: "hearts"}, {point: 6; suit: "diamonds"},{point: 6; suit: "clubs"},{point: 6; suit: "spade"},
+// {point: 5; suit: "hearts"}, {point: 5; suit: "diamonds"},{point: 5; suit: "clubs"},{point: 5; suit: "spade"},
+// {point: 4; suit: "hearts"}, {point: 4; suit: "diamonds"},{point: 4; suit: "clubs"},{point: 4; suit: "spade"},
+// {point: 3; suit: "hearts"}, {point: 3; suit: "diamonds"},{point: 3; suit: "clubs"},{point: 3; suit: "spade"},
+// {point: 2; suit: "hearts"}, {point: 2; suit: "diamonds"},{point: 2; suit: "clubs"},{point: 2; suit: "spade"},
+// {point: 1; suit: "hearts"}, {point: 1; suit: "diamonds"},{point: 1; suit: "clubs"},{point: 1; suit: "spade"}
+// ];
+// var cards = []
+// var counts = 0;
